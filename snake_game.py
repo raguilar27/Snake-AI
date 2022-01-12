fb6f244189bd15cf2e7b0c2d91906d10c7b40110
Rogelio <raguilar27@github.com>
2022-01-11 19:53:23 -0800

    updated ui and clock settings

--- snake_game.py
+++ snake_game.py
@@ -4,18 +4,26 @@ from enum import Enum
 from collections import namedtuple
 
 pygame.init()
+font = pygame.font.Font('arial.ttf', 25)
 
-
-class Direction(Enum()):
+class Direction(Enum):
     RIGHT = 1
     LEFT = 2
     UP = 3
     DOWN = 4
 
 
-Point = namedtuple('Point', 'x', 'y')
+Point = namedtuple('Point', 'x, y')
+
+# rgb colors
+WHITE = (255, 255, 255)
+RED = (200, 0, 0)
+BLUE1 = (0, 0, 255)
+BLUE2 = (0, 100, 255)
+BLACK = (0, 0, 0)
 
 BLOCK_SIZE = 20
+SPEED = 40
 
 
 class SnakeGame:
@@ -24,7 +32,7 @@ class SnakeGame:
         self.w = w
         self.h = h
         # init display
-        self.display = pygame.display.set_mode(self.w, self.h)
+        self.display = pygame.display.set_mode((self.w, self.h))
         pygame.display.set_caption('Snake')
         self.clock = pygame.time.Clock()
 
@@ -32,7 +40,7 @@ class SnakeGame:
         self.direction = Direction.RIGHT
 
         self.head = Point(self.w/2, self.h/2)
-        self.snake = [self.head, 
+        self.snake = [self.head,
                       Point(self.head.x-BLOCK_SIZE, self.head.y),
                       Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
 
@@ -40,16 +48,43 @@ class SnakeGame:
         self.food = None
         self.place_food()
 
-    
+
     def place_food(self):
         x = random.randint(0, (self.w-BLOCK_SIZE)//BLOCK_SIZE) * BLOCK_SIZE
         y = random.randint(0, (self.w-BLOCK_SIZE)//BLOCK_SIZE) * BLOCK_SIZE
-        self.food = Point(x,y)
+        self.food = Point(x, y)
         if self.food in self.snake:
             self.place_food()
 
     def play_step(self):
+        # 1. collect user input
+
+        # 2. move
+
+        # 3. check if game over
+
+        # 4. place new food or just move
+
+        # 5. update ui and clock
+        self.update_ui()
+        self.clock.tick(SPEED)
+        # 6. return game over and score
+        game_over = False
+        return game_over, self.score
+
+
+    def update_ui(self):
+        self.display.fill(BLACK)
         
+        for pt in self.snake:
+            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
+            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
+
+        pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
+        
+        text = font.render("Score: " + str(self.score), True, WHITE)
+        self.display.blit(text, [0, 0])
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
@@ -57,9 +92,11 @@ if __name__ == '__main__':
 
     # game loop
     while True:
-        game.play_step()
+        game_over, score = game.play_step()
 
-        # break if game over
+        if game_over == True:
+            break
 
+    print('Final Score', score)
 
     pygame.quit()
