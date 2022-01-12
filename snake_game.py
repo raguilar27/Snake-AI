9d18b4cfa8704cc48908c07e8ab10225eb886900
Rogelio <raguilar27@github.com>
2022-01-11 18:40:51 -0800

    setup display

--- snake_game.py
+++ snake_game.py
@@ -1,7 +1,22 @@
 import pygame
 import random
+from enum import Enum
+from collections import namedtuple
 
 pygame.init()
+
+
+class Direction(Enum()):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
+
+Point = namedtuple('Point', 'x', 'y')
+
+BLOCK_SIZE = 20
+
 
 class SnakeGame:
 
@@ -9,8 +24,12 @@ class SnakeGame:
         self.w = w
         self.h = h
         # init display
+        self.display = pygame.display.set_mode(self.w, self.h)
+        pygame.display.set_caption('Snake')
+        self.clock = pygame.time.Clock()
 
         # init game state
+
 
     def play_step(self):
         pass
