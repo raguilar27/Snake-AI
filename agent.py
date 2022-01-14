586273410be24579887c2c1619fd7476757b360e
Rogelio <raguilar27@github.com>
2022-01-14 12:53:10 -0800

    added a plot helper to plot the scores of snake ai

--- agent.py
+++ agent.py
@@ -4,6 +4,7 @@ import numpy as np
 from collections import deque
 from snake_game import SnakeGameAI, Direction, Point
 from model import Linear_QNet, QTrainer
+from helper import plot
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
@@ -125,7 +126,7 @@ def train():
 
         if done:
             # train long memory, plot result
-            game.rest()
+            game.reset()
             agent.n_games += 1
             agent.train_long_memory()
 
@@ -135,7 +136,11 @@ def train():
 
             print("Game", agent.n_games, "Score", score, "Record", record)
 
-            # TODO: plot
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
 
 
 if __name__ == '__main__':
